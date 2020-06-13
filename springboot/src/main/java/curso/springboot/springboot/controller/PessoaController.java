b7687f54f461a76102d78a4104259609368dba57
Fabio <fabio.meihua08@gmail.com>
2020-06-13 15:44:22 -0300

    Primeira parte implementacao completa Spring + Materialize CSS + PostgreSQL

--- springboot/src/main/java/curso/springboot/springboot/controller/PessoaController.java
+++ springboot/src/main/java/curso/springboot/springboot/controller/PessoaController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import curso.springboot.model.Pessoa;
@@ -72,7 +74,17 @@ public class PessoaController {
 		modelAndView.addObject("pessoaobj", new Pessoa());//retorna objeto vazio para exibir na tela
 		return modelAndView;
 }
-
+		
+		@PostMapping("**/pesquisarpessoa")
+		public ModelAndView pesquisar(@RequestParam("nomepesquisa")String nomepesquisa) {
+			ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+			modelAndView.addObject("pessoas", pessoaRepository.findPessoaByName(nomepesquisa));
+			modelAndView.addObject("pessoaobj", new Pessoa());
+			return modelAndView;
+		}
+		
+		
+		
 		}
 
 
