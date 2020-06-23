963a0da414b44872314ccf794fddfe169b902dcc
fabio <fabio.meihua08@gmail.com>
2020-06-23 04:04:20 -0300

    final

--- springboot/src/main/java/curso/springboot/springboot/SpringbootApplication.java
+++ springboot/src/main/java/curso/springboot/springboot/SpringbootApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@SuppressWarnings("unused")
 @SpringBootApplication
 @EntityScan(basePackages = "curso.springboot.model")
 @ComponentScan(basePackages = {"curso.*"})
--- springboot/src/test/java/curso/springboot/springboot/SpringbootApplicationTests.java
+++ springboot/src/test/java/curso/springboot/springboot/SpringbootApplicationTests.java
@@ -2,10 +2,10 @@ package curso.springboot.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.repository.CrudRepository;
+
 
 import curso.springboot.model.Pessoa;
-import curso.springboot.repository.PessoaRepository;
+
 
 @SpringBootTest
 class SpringbootApplicationTests {
