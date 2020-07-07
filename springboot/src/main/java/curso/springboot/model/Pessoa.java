530f5e71e4db1681554832e714af376b6b409c8f
Fabio <fabio.meihua08@gmail.com>
2020-07-07 19:01:25 -0300

    Updates na tela de cadastro

--- springboot/src/main/java/curso/springboot/model/Pessoa.java
+++ springboot/src/main/java/curso/springboot/model/Pessoa.java
@@ -1,18 +1,25 @@
 package curso.springboot.model;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 public class Pessoa implements Serializable {
@@ -47,9 +54,32 @@ public class Pessoa implements Serializable {
 	@ManyToOne
 	private Profissao profissao;
 	
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	@Temporal(TemporalType.DATE)
+	private Date dataNascimento;
 	
 	
 	
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	@Enumerated(EnumType.STRING)
+	private Cargo cargo;
+	
+	
+	public Cargo getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(Cargo cargo) {
+		this.cargo = cargo;
+	}
+
 	public Profissao getProfissao() {
 		return profissao;
 	}
